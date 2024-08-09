6a513435b05200d86b394e7ed7c2e60db34c4cb2
Alexander Gomez <alexgomezgomez15@gmail.com>
2024-08-09 14:41:11 -0400

    Course Services complete

--- src/main/java/sba/sms/services/CourseService.java
+++ src/main/java/sba/sms/services/CourseService.java
@@ -1,8 +1,11 @@
 package sba.sms.services;
 
+import jakarta.persistence.TypedQuery;
 import org.hibernate.HibernateException;
 import org.hibernate.Session;
+import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
 import org.hibernate.query.Query;
 import sba.sms.dao.CourseI;
 import sba.sms.models.Course;
@@ -18,18 +21,86 @@ import java.util.List;
  */
 public class CourseService implements CourseI {
 
+    SessionFactory factory = null;
+    Session session = null;
+    Transaction transaction = null;
+
     @Override
     public void createCourse(Course course) {
+        try{
+            factory = new Configuration().configure().buildSessionFactory();
+            session = factory.openSession();
+            transaction = session.beginTransaction();
 
+            session.persist(course);
+
+            transaction.commit();
+
+        }catch (Exception e){
+            if(transaction != null){
+                transaction.rollback();
+            }
+            e.printStackTrace();
+        }finally {
+            factory.close();
+            session.close();
+        }
     }
 
     @Override
     public Course getCourseById(int courseId) {
-        return null;
+
+        Course course = null;
+
+        try{
+            factory = new Configuration().configure().buildSessionFactory();
+            session = factory.openSession();
+            transaction = session.beginTransaction();
+
+            course = session.get(Course.class, courseId);
+
+            transaction.commit();
+
+        }catch (Exception e){
+            if(transaction != null){
+                transaction.rollback();
+            }
+            e.printStackTrace();
+        }finally {
+            factory.close();
+            session.close();
+        }
+
+        return course;
     }
 
     @Override
     public List<Course> getAllCourses() {
-        return List.of();
+
+        try{
+            factory = new Configuration().configure().buildSessionFactory();
+            session = factory.openSession();
+            transaction = session.beginTransaction();
+
+            String hql = "FROM course";
+            TypedQuery<Course> query = session.createNamedQuery(hql,
+                    Course.class);
+            List<Course> courses = query.getResultList();
+
+            transaction.commit();
+
+            return courses;
+
+        }catch (Exception e){
+            if(transaction != null){
+                transaction.rollback();
+            }
+            e.printStackTrace();
+        }finally {
+            factory.close();
+            session.close();
+        }
+
+        return null;
     }
 }
--- src/main/java/sba/sms/services/StudentService.java
+++ src/main/java/sba/sms/services/StudentService.java
@@ -1,9 +1,12 @@
 package sba.sms.services;
 
+import jakarta.persistence.TypedQuery;
 import lombok.extern.java.Log;
 import org.hibernate.HibernateException;
 import org.hibernate.Session;
+import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
 import org.hibernate.query.NativeQuery;
 import org.hibernate.query.Query;
 import sba.sms.dao.StudentI;
@@ -21,6 +24,100 @@ import java.util.List;
  * generate a logger file.
  */
 
-public class StudentService {
+public class StudentService implements StudentI{
 
+    SessionFactory factory = null;
+    Session session = null;
+    Transaction transaction = null;
+
+    @Override
+    public List<Student> getAllStudents() {
+        try{
+            factory = new Configuration().configure().buildSessionFactory();
+            session = factory.openSession();
+            transaction = session.beginTransaction();
+
+            String hql = "FROM student";
+            TypedQuery<Student> query = session.createNamedQuery(hql,
+                    Student.class);
+            List<Student> students = query.getResultList();
+
+            transaction.commit();
+
+            return students;
+
+        }catch (Exception e){
+            if(transaction != null){
+                transaction.rollback();
+            }
+            e.printStackTrace();
+        }finally {
+            factory.close();
+            session.close();
+        }
+
+        return null;
+    }
+
+    @Override
+    public void createStudent(Student student) {
+        try{
+            factory = new Configuration().configure().buildSessionFactory();
+            session = factory.openSession();
+            transaction = session.beginTransaction();
+
+            session.persist(student);
+
+            transaction.commit();
+
+        }catch (Exception e){
+            if(transaction != null){
+                transaction.rollback();
+            }
+            e.printStackTrace();
+        }finally {
+            factory.close();
+            session.close();
+        }
+    }
+
+    @Override
+    public Student getStudentByEmail(String email) {
+
+        try{
+            factory = new Configuration().configure().buildSessionFactory();
+            session = factory.openSession();
+            transaction = session.beginTransaction();
+
+           Student student = session.get(Student.class, email);
+
+            transaction.commit();
+            return student;
+        }catch (Exception e){
+            if(transaction != null){
+                transaction.rollback();
+            }
+            e.printStackTrace();
+        }finally {
+            factory.close();
+            session.close();
+        }
+
+        return null;
+    }
+
+    @Override
+    public boolean validateStudent(String email, String password) {
+        return false;
+    }
+
+    @Override
+    public void registerStudentToCourse(String email, int courseId) {
+
+    }
+
+    @Override
+    public List<Course> getStudentCourses(String email) {
+        return List.of();
+    }
 }
